3686ad6be9f764c37da68bbc24feb9dc1e37641e
Ilya Tikhonov <altaph@mail.ru>
2023-03-16 19:44:29 +0500

    via class

--- weather.py
+++ weather.py
@@ -11,8 +11,6 @@ url = {
     "template" : "https://wttr.in/{}",
     "payload"  : {"T": "", "lang": "ru", "M": "", "n":"", "q":""}
 }
-#url_payload = {"T": "", "lang": "ru", "M": "", "n":"", "q":""}
-#url["payload"] = {"T": "", "lang": "ru", "M": "", "n":"", "q":""}
 
 cities = ['London', 'svo','Череповец']
 for city in cities:
