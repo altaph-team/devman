b1baba64f3c06cdddf9c4975415e80c7682f8495
Ilya Tikhonov <altaph@mail.ru>
2023-03-19 19:36:24 +0500

    pep8

--- weather.py
+++ weather.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class WeatherService:
     def __init__(self, url):
         self.__url = url
@@ -16,21 +17,19 @@ class WeatherService:
 
     def get_weather(self, city):
         # явно преобразуем в string. чтобы убрать сообщение об ошибке
-        url_weather = str(self.get_url_template()).format(city)   
+        url_weather = str(self.get_url_template()).format(city)
 
         response = requests.get(url_weather, self.get_url_params())
         response.raise_for_status()
-    
         return response.text
 
-
 url = {
-    "template" : "https://wttr.in/{}",
+    "template": "https://wttr.in/{}",
     # см. https://wttr.in/help
-    "payload"  : {"T": "", "lang": "ru", "M": "", "n":"", "q":""}
+    "payload": {"T": "", "lang": "ru", "M": "", "n": "", "q": ""}
 }
 
-cities = ['London', 'svo','Череповец']
+cities = ['London', 'svo', 'Череповец']
 weather = WeatherService(url)
 
 print(*[weather.get_weather(city) for city in cities])
